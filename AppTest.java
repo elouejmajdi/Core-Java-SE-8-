e6a06c435c75b3064216646cc5c1bf6b956b1645
Majdi Elouej <41271175+elouejmajdi@users.noreply.github.com>
2018-10-28 12:19:36 -0400

    Add files via upload

--- AppTest.java
+++ AppTest.java
@@ -1,37 +1,57 @@
-
+package com.elouej.tutorial;
 
 import java.util.Scanner;
+
 public class AppTest {
 
-	
-	
-	
-	
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in); 
+/*		
 		
-	System.out.println("Enter the new word:  ");
+		
+		Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+				You may assume that each input would have exactly one solution, and you may not use the same element twice
+				
+				Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+
+*/
+
+		
+		
+		
+		
+		
+		int[] nums = {2, 7, 11, 15};
+		int target=26 ; 
+		int [] list = new int[2]; 
+		
+		
+		
+		for(int i=0;i<nums.length;i++) {
+			for(int j=i+1;j<nums.length;j++)
+			{
+				if((nums[i]+nums[j])==target)
+				{
+				list[0]=i;
+				list[1]=j;
+				}
+			}
+		}
+		
+		
+		
 	
-	String str=sc.nextLine();
-	
-//		  String str="klashfuiosao176126";
-	        String nsNumber="-0123456789";
-	        String ns ="";
-	        
-	        for(int i=0;i<str.length();i++)
-	        {
-	            for(int j=0;j<nsNumber.length();j++)
-	            {
-	                if(str.charAt(i)==nsNumber.charAt(j))
-	                {
-	                    ns +=nsNumber.charAt(j);
-	                }
-	                    
-	            }
-	        }
-	       	
-	        System.out.println(Integer.parseInt(ns));
+		System.out.println("[ "+list[0]+" , " +list[1]+" ]");
+		
+		
 	}
+
 }
